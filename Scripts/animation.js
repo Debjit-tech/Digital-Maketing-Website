9e9f02c683c9f810fbfcc53248993560d740b91a
parikshitdas10 <69421856+parikshitdas10@users.noreply.github.com>
2020-10-01 00:19:06 +0530

    dark mode code
    customize the position of toggle button, you may also change the color of the button.

--- Scripts/animation.js
+++ Scripts/animation.js
@@ -88,3 +88,20 @@ var scene5 = new ScrollMagic.Scene({
 })
 .setClassToggle('.backtotop','visible')
 .addTo(controller5);
+//dark mode
+const options = {
+  bottom: "32px", // default: '32px'
+  right: "32px", // default: '32px'
+  left: "unset", // default: 'unset'
+  time: "0.5s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "#fff", // default: '#fff'
+  buttonColorDark: " ##00ffff", // default: '#100f2c'
+  buttonColorLight: "#00ffff", // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: "🌓", // default: ''
+  autoMatchOsTheme: true, // default: true
+};
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
